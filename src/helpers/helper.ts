799959f53ef5725fef811993a7537a09eba3be74
Tony <code.monkey023@gmail.com>
2021-06-10 17:03:46 +0200

    added get user helper

--- src/helpers/helper.ts
+++ src/helpers/helper.ts
@@ -1,14 +1,17 @@
 
-import { TextChannel } from "discord.js";
+import { TextChannel, User } from "discord.js";
 import { client } from "../main";
 
 // Public helper get functions
 export const helpers = {
     get : {
-        channel : function(client : Client,channelId: string) {
+        channel : function(channelId: string) {
             return (client.channels.cache.get(channelId) as TextChannel);
         },
-        user: function(client)
+        user: function(user: User) {
+            return (client.users.cache.get(user.id) as User);
+
+        }
     },
     isRole : {
         owner : function(userId: string) {
