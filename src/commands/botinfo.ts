3273a591c6bbc52e679a054a4a6d034e11815f19
Tony Moonen <tony.drum01@gmail.com>
2021-07-26 00:48:51 +0200

    Added bot creation date to botinfo command.

--- src/commands/botinfo.ts
+++ src/commands/botinfo.ts
@@ -1,4 +1,5 @@
-import { Message, Command, MessageEmbed, DataResolver } from "discord.js";
+import { Message, Command, MessageEmbed, User} from "discord.js";
+import { helpers } from "../helpers/helper";
 
 const command : Command = {
     name: 'botinfo',
@@ -8,17 +9,33 @@ const command : Command = {
         const timestamp: number = require("../data/runtime.json").data.startupDate;
         let dateNow: Date = new Date(timestamp * 1000);
 
-        const date: number = dateNow.getDate();
-        const month: number = dateNow.getMonth()+1;
-        const year: number = dateNow.getFullYear();
+        const startupDate: number = dateNow.getDate();
+        const startupMonth: number = dateNow.getMonth()+1;
+        const startupYear: number = dateNow.getFullYear();
         
-        const hours: string = dateNow.getHours().toString();
-        const minutes: string = "0" + dateNow.getMinutes();
-        const seconds: string = "0" + dateNow.getSeconds();
+        const startupHours: string = dateNow.getHours().toString();
+        const startupMinutes: string = "0" + dateNow.getMinutes();
+        const startupSeconds: string = "0" + dateNow.getSeconds();
+
+        const bot : User = message.client.user;
+
+        let dateBot: Date = new Date(bot.createdTimestamp);
+
+        const creationDate: number = dateBot.getDate();
+        const creationMonth: number = dateBot.getMonth()+1;
+        const creationYear: number = dateBot.getFullYear();
+        
+        const creationHours: string = dateBot.getHours().toString();
+        const creationMinutes: string = "0" + dateBot.getMinutes();
+        const creationSeconds: string = "0" + dateBot.getSeconds();
+
+        const weenisAvatar: string = helpers.get.weenis(message.client).displayAvatarURL({dynamic: true});
         
         infoEmbed.setTitle('Bot info')
-            .setThumbnail(message.client.user.displayAvatarURL())
-            .addField("⌛ Uptime", `Bot has been up since ${date}/${month}/${year} ${hours}:${minutes.substr(-2)}:${seconds.substr(-2)}`, true)
+            .setThumbnail(message.client.user.displayAvatarURL({dynamic: true}))
+            .addField("⌛ Uptime", `Bot has been up since ${startupDate}/${startupMonth}/${startupYear} ${startupHours}:${startupMinutes.substr(-2)}:${startupSeconds.substr(-2)}`)
+            .addField("🔨 Creation", `${bot.username} was born into this world on ${creationDate}/${creationMonth}/${creationYear} ${creationHours}:${creationMinutes.substr(-2)}:${creationSeconds.substr(-2)}`)
+            .setFooter("Bot made by weenis#7144.", `${weenisAvatar}`);
         return message.channel.send(infoEmbed);
     },
 };
